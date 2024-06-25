8ee01f26a62c1cedc0f6a09ccddd05cff5e9df15
sw1 <stephenwoloszynek@gmail.com>
2024-06-24 21:32:45 -0400

    added then removed imp w for balancing

--- code/15_lasso_treelabs.R
+++ code/15_lasso_treelabs.R
@@ -58,7 +58,7 @@ res <- tibble(labs) %>%
   mutate(label=if_else(is.na(label),'onlyexpert',label),
          threshold=as.integer(str_extract(threshold,'[0-9]+')),
          fraction=as.integer(str_extract(fraction,'[0-9]+'))) %>%
-  mutate(b_acc=0,prec=0,rec=0,f1=0,prop1=0)
+  mutate(lambda=0,b_acc=0,prec=0,rec=0,f1=0,prop1=0)
 
 for (i in seq_along(labs)){
   
@@ -70,68 +70,68 @@ for (i in seq_along(labs)){
     select(id,y=all_of(lab)) %>%
     filter(y != -1)
   
+  # calculate weights for class imbalance
+  w <- nrow(y_train)/(table(y_train$y) * length(unique(y_train$y)))
+  #w <- 1/(table(y_train$y)/nrow(y_train))
+  w <- w/sum(w)
+  w <- w[y_train$y+1]
+  
   x_train <- dv_train[as.character(y_train$id),]
   colnames(x_train) <- paste0('f',1:ncol(x_train))
   
   x_train <- as_tibble(x_train) %>%
+    #bind_cols(tibble(w=as.numeric(w))) %>%
+    #mutate(w=importance_weights(w)) %>%
     bind_cols(y=as.factor(unname(y_train$y)))
-  
-  # calculate weights for class imbalance
-  # w <- nrow(y_train)/(table(y_train$y) * length(unique(y_train$y)))
-  # w <- w[y_train$y+1]
-  
+    
   set.seed(s)
   
-  lambda <- logistic_reg(
-    penalty = tune(),
-    mixture = 0.5,
-  ) %>%
+  mod <- logistic_reg(penalty = tune(),mixture = 0.5) %>%
     set_engine('glmnet') %>%
-    set_mode('classification') %>%
-    tune_grid(
-      recipe(y ~ .,data=x_train) ,
-      resamples=vfold_cv(x_train,strata=y,v=all_cores),
-      grid=5,
-      metrics=metric_set(bal_accuracy)) %>% 
+    set_mode('classification')
+    
+  lambda <- workflow() %>%
+    add_model(mod) %>%
+    #add_recipe(y ~ .,data=x_train) %>%
+    add_formula(y ~ .) %>%
+    #add_case_weights(w) %>%
+    tune_grid(resamples=vfold_cv(x_train,strata=y,v=all_cores),
+              grid=5,
+              metrics=metric_set(bal_accuracy)) %>% 
     select_best(metric='bal_accuracy') %>%
     pull(penalty)
   
-  lasso <- logistic_reg(
-    penalty = !!lambda,
-    mixture = 0.5
-  ) %>%
+  mod <- logistic_reg(penalty=!!lambda,mixture = 0.5) %>%
     set_engine('glmnet') %>%
     set_mode('classification')
   
   wf <- workflow() %>%
     add_formula(y ~ .) %>%
-    add_model(lasso) 
-  
-  train_cv <- vfold_cv(x_train %>%
-                         mutate(
-                           y=as.factor(if_else(
-                             y == 1,'pos','neg'))),
-                       strata=y,v=all_cores)
+    #add_case_weights(w) %>%
+    add_model(mod) 
+    
   
   cal <- wf %>%
-    fit_resamples(train_cv,metrics=metric_set(roc_auc,brier_class),
+    fit_resamples(vfold_cv(x_train %>%
+                             mutate(y=as.factor(if_else(
+                               y == 1,'pos','neg'))),
+                           strata=y,v=all_cores),
+                  metrics=metric_set(roc_auc,brier_class),
                   control=control_resamples(save_pred=TRUE)) %>%
     cal_estimate_logistic()
   
   y_hat <- wf %>%
     fit(data=x_train %>%
-          mutate(
-            y=as.factor(if_else(
-              y == 1,'pos','neg')))) %>%
+          mutate(y=as.factor(if_else(y == 1,'pos','neg')))) %>%
     augment(new_data=x_test %>%
-              mutate(y=as.factor(if_else(
-                y == 1,'pos','neg')))) %>%
+              mutate(y=as.factor(if_else(y == 1,'pos','neg')))) %>%
     cal_apply(cal,pred_class=.pred_class) %>%
     select(pred_1=.pred_pos)
   
   tbl_yhat <- tibble(id=y_test$id,
                      pred=if_else(y_hat[,1] > 0.5,1,0)) %>%
-    left_join(y_test,by='id')
+    mutate(pred=factor(pred,levels=0:1)) %>%
+    left_join(y_test %>% mutate(y=factor(y,level=0:1)),by='id')
   
   conf <- confusionMatrix(table(tbl_yhat$y,tbl_yhat$pred),
                           mode='everything',
@@ -145,13 +145,15 @@ for (i in seq_along(labs)){
   names(perf) <- c('b_acc','prec','rec','f1','prop1')
   
   res <- res %>%
-    rows_update(as_tibble_row(perf) %>% mutate(fn=lab),by='fn')
+    rows_update(as_tibble_row(perf) %>% 
+                  mutate(lambda=lambda,
+                         fn=lab),
+                by='fn')
   
   print(res %>% select(-fn),n=Inf)
   
 }
 
 write_csv(res,file.path(path,'res','15_lasso_results.csv.gz'))
-write_rds(fits,file.path(path,'res','15_lasso_fits.rds'))
 
 stopCluster(cl)
